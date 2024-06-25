bfe3e79d701c6d0de85ed590b06234a44bc44eb7
LeeJuYeop <wnduq330@naver.com>
2024-06-25 21:28:36 +0900

    leader before pull

--- example2.cpp
+++ example2.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+class Myclass
+{
+public:
+    int m;
+    int n;
+};
+
+int main()
+{
     int a = 0;
     cout << a << endl;
     return 0;
